331070d1a0f2678b8d404948f9a4d4b76a179ba7
Lara van Diemen <laravandiemen23@gmail.com>
2019-05-27 10:45:59 +0200

    Login Edit

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+import TopBar from '../layout/TopBar';
+
+import './Login.css';
 
 class Login extends React.Component{
   render() {
     return(
-      <Link to="/search">Doorgaan </Link>
+      <div>
+        <TopBar />
+        <div className="LoginPageContainer">
+          <form className="form">
+          
+          </form>
+          <Link to="/search">Doorgaan </Link>
+        </div>
+      </div>
     )
   }
 }
