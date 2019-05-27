c575723c47a84acea18228451f5ad22bd2664b2b
Lara van Diemen <laravandiemen23@gmail.com>
2019-05-27 11:10:11 +0200

    Login css

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -13,7 +13,17 @@ class Login extends React.Component{
         <TopBar />
         <div className="LoginPageContainer">
           <form className="form">
-          
+            <div className="containerFormItem" id="gebruikersnaam" >
+              <label className="label">Gebruikersnaam</label>
+              <input className="inputGebruikersnaam" autoFocus type="text" />
+            </div>
+            <div className="containerFormItem" id="wachtwoord" >
+              <label className="label">Wachtwoord</label>
+              <input className="inputWachtwoord" type="password" />
+            </div>
+            <div>
+              <input className="loginButton" type="submit" value="Login" />
+            </div>
           </form>
           <Link to="/search">Doorgaan </Link>
         </div>
