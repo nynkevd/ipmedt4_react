5512a4fa5f29c8b71aafb9b22a62e5de2132709b
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-05-30 21:55:19 +0200

    "Verwijder" interest

---
+++ src/components/editInterests/MyInterests.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import './MyInterests.css';
+
+class MyInterests extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  deleteOnClick = (event) => {
+    var interestsList = document.getElementById("interestsList").childNodes;
+
+    var deleteItem = document.getElementById(event.target.id).id;
+    console.log(document.getElementById(deleteItem));
+    document.getElementById(deleteItem).parentElement.setAttribute("class", "hidden")
+    //var interest = document.getElementById(event.target.id);
+    console.log("Verwijder: " + deleteItem);
+  }
+
+  render() {
+    return (
+      <div id="interestsList">
+      <ul className="interestsList">
+        {
+          this.props.interests.map((interest, index) =>
+            <li className="interest" key={index}>{interest}  <img onClick={this.deleteOnClick} id={interest} className="icon" src="./img/icons/pencil-edit-button.svg" alt=""/></li>
+          )
+        }
+      </ul>
+      </div>
+    )
+  }
+}
+
+export default MyInterests;
---
+++ src/components/pages/InterestsEdit.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import TopBar from '../layout/TopBar';
+import MyInterests from '../editInterests/MyInterests';
+
+import './InterestsEdit.css';
+
+import axios from "axios";
+
+class InterestsEdit extends React.Component {
+  state = { myInterests:[] }
+  BASE_URL = "http://136.144.230.97:8080/api/interests/";
+  TEMP_URL = "localhost:8000/api/interests/"
+  username = "Nynke"; //Moet aangepast worden naar de ingelogde gebruiker
+  api_token = "?api_token=rx7Mi675A1WDEvZPsGnrgvwkCEeOKlrX7rIPoXocluBKnupp9A02OLz7QcSL";
+
+  componentDidMount(){
+    this.getUserInterests();
+  }
+
+  getUserInterests = () => {
+    //Lijst van de huidige interesses opvragen
+    axios.get(this.BASE_URL + this.username + this.api_token).then(res =>{
+      console.log(res);
+      this.setState({myInterests: res.data})
+    })
+  }
+
+  render(){
+    return(
+    <div>
+      <TopBar />
+      <div className="interestPageContainer">
+        <h1> Mijn interesses </h1>
+        <MyInterests interests={this.state.myInterests}> </ MyInterests>
+      </div>
+    </div>
+  )}
+
+}
+
+export default InterestsEdit;
