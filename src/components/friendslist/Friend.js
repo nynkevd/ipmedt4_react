06b42a3b7f20717ce0d411531c8ed7110fe793ed
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-06-06 16:36:22 +0200

    Friends op basis van ingelogde user

---
+++ src/components/friendslist/Friend.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+import InterestsFriends from "./InterestsFriends";
+import ReistrajectFriends from "./ReistrajectFriends";
+import UserinfoFriends from "./UserinfoFriends";
+
+import './UserinfoFriends.css';
+
+const base_url = "http://136.144.230.97:8080/api/";
+const api_token = "?api_token=rx7Mi675A1WDEvZPsGnrgvwkCEeOKlrX7rIPoXocluBKnupp9A02OLz7QcSL";
+
+class Friend extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: props.username,
+      profilepicture: null,
+    }
+  }
+
+  componentDidMount(){
+    this.getUserInfoFromApi();
+  }
+
+  getUserInfoFromApi(){
+    axios.get(base_url + "userinfo/" + this.state.username + api_token).then(res => {
+      this.setState({profilepicture: res.data.picture});
+    });
+  }
+
+  render(){
+    console.log(this.state.profilepicture);
+    return(
+      <div className="friend">
+        <UserinfoFriends profielfoto={this.state.profilepicture} naam={this.state.username}></UserinfoFriends>
+        <hr className="LineBetweenFriends"/>
+      </div>
+    )
+  }
+}
+
+export default Friend;
---
+++ src/components/friendslist/UserFriendsList.js
@@ -0,0 +1,52 @@
+//React en benodigheden importeren
+import React from 'react';
+import axios from "axios";
+//Redux importeren
+import { connect } from "react-redux";
+import {
+  changeUserName,
+} from "./../../actions";
+//Eigen componenten importeren
+import Friend from './Friend';
+
+const base_url = "http://136.144.230.97:8080/api/";
+const api_token = "?api_token=rx7Mi675A1WDEvZPsGnrgvwkCEeOKlrX7rIPoXocluBKnupp9A02OLz7QcSL";
+
+class UserFriendsList extends React.Component {
+  state = {
+    friends: [],
+  }
+
+  componentDidMount(){
+    this.getFriendsFromApi(base_url, api_token);
+  }
+
+  getFriendsFromApi = (base_url, api_token) => {
+    axios.get(base_url + "friends/" + this.props.userName + api_token).then(res => {
+      this.setState({friends: res.data});
+    });
+  }
+
+  render(){
+    console.log(this.state.friends);
+    return (
+      <div>
+      {
+        this.state.friends.map((username, index) =>
+          <Friend username={username} key={index} index={index}/>
+        )
+      }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state =>{
+  return {
+    userName: state.userName,
+  };
+}
+
+export default connect(mapStateToProps,{
+  changeUserName: changeUserName,
+})(UserFriendsList);
