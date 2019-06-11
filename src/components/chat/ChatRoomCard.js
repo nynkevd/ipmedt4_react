7ce97ec3c716f2e4d2403c0e656404179bd68914
Anouk <aananouk@gmail.com>
2019-06-11 13:41:59 +0200

    Chat overzicht tijdstip en laatste bericht (nog niet af)

--- src/components/chat/ChatRoomCard.js
+++ src/components/chat/ChatRoomCard.js
@@ -20,7 +20,62 @@ import './ChatRoomCard.css';
 class ChatRoomCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {id:props.id};
+    this.state = {
+      id:props.id,
+      lastMessage: "",
+      lastMessageTime: "",
+    };
+  }
+
+  componentDidMount(){
+    this.getLastMessage();
+  }
+
+  getLastMessage(){
+    this.props.chatKitUser.fetchMessages({
+      roomId: this.props.room.id,
+      direction: 'older',
+      limit: 1,
+    }).then(messages => {
+      this.getLastMessageTime(messages[0]);
+
+      this.setState({
+        lastMessage: messages[0].text,
+      })
+    })
+  }
+
+  getLastMessageTime(message){
+    // Deze functie is echt veeel te lang
+    var createdAt = message.createdAt;
+
+    // Datum van het laatste bericht
+    var date = createdAt.split('T')[0].split('-');
+    date[0] = parseInt(date[0], 10);
+    date[1] = parseInt(date[1], 10);
+    date[2] = parseInt(date[2], 10);
+
+    // Tijdstip van het bericht
+    var time = createdAt.split('T')[1].split('Z')[0].split(':');
+    // timezone loopt 2 uur achter
+    // Zet een 0 voor de tijd
+    time[0] = ("0" + time[0]).slice(-2);
+    time[1] = ("0" + time[1]).slice(-2);
+
+    // Datum van vandaag
+    var d = new Date();
+
+    // Als het de datum van vandaag is wordt alleen de tijd weergegeven
+    if(date[0] === d.getFullYear() && date[1] === (d.getMonth() + 1) && date[2] === d.getDate()){
+      this.setState({
+        lastMessageTime: time[0] + ":" + time[1],
+      });
+    }else{
+      // Anders wordt de datum weergegeven
+      this.setState({
+        lastMessageTime: date[2] + "-" + date[1] + "-" + date[0],
+      });
+    }
   }
 
   onClick = event => {
@@ -32,10 +87,11 @@ class ChatRoomCard extends React.Component {
       <Link to='/chatRoom' className="chatCardLink">
         <div className="chatCardContainer" onClick={this.onClick}>
           <div>
-            <h3 className="chatCardContainer__text">{getRoomName(this.props.room, this.props.chatKitUser)}</h3>
-            <p className="chatCardContainer__text">{"Laatste bericht om: " +  this.props.room.lastMessageAt}</p>
-            
-            // unreadCount is een varaibele die door chatkit wordt bijgehouden met het aantal ongelezen berichten
+            <h3 className="chatCardContainer__name">{getRoomName(this.props.room, this.props.chatKitUser)}</h3>
+            <p className="chatCardContainer__message">{this.state.lastMessage}</p>
+            <span className="chatCardContainer__time">{this.state.lastMessageTime}</span>
+
+            {/* unreadCount is een varaibele die door chatkit wordt bijgehouden met het aantal ongelezen berichten*/}
             <UnreadMessageCount unreadMessages={this.props.room.unreadCount}/>
           </div>
         </div>
