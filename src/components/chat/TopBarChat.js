402272f328d2c0ae6fea6bf0eef75a7ace42d26d
Lara van Diemen <lara23vd@gmail.com>
2019-06-14 10:57:33 +0200

    Profielfoto toegevoegd aan de TopBarChat van de betreffend gebruiker

--- src/components/chat/TopBarChat.js
+++ src/components/chat/TopBarChat.js
@@ -1,6 +1,7 @@
 //React importeren
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 // Redux importeren
 import { connect } from 'react-redux';
 import {
@@ -12,16 +13,34 @@ import {getRoomName} from './methodsChat.js';
 //CSS importeren
 import './TopBarChat.css';
 
+const base_url = "http://136.144.230.97:8080/api/";
+const api_token = "?api_token=rx7Mi675A1WDEvZPsGnrgvwkCEeOKlrX7rIPoXocluBKnupp9A02OLz7QcSL";
 
-const TopBarChat = (props) =>{
-  return(
-    <div className="topBarChat">
-      <Link to='/chat' className="chatCardLink">
-      <img className="topBarChat__back" src="./img/icons/arrow_back.svg" alt="Terug"/>
-      </Link>
-      <p className="topBarChat__text">{getRoomName(props.clickedChatroom, props.chatKitUser)}</p>
-    </div>
-  );
+class TopBarChat extends React.Component {
+  state = {picture: null}
+
+  componentDidMount(){
+    this.getProfilePicture();
+    console.log(this.state.picture);
+  }
+
+  getProfilePicture = () =>{
+    axios.get(base_url + "userinfo/" + this.props.clickedChatroom.createdByUserId + api_token).then(res => {
+      this.setState({picture: res.data.picture});
+    });
+  }
+
+  render(){
+    return(
+      <div className="topBarChat">
+        <Link to='/chat' className="chatCardLink">
+          <img className="topBarChat__back" src="./img/icons/arrow_back.svg" alt="Terug"/>
+        </Link>
+        <img className="topBarChat__img" src={this.state.picture} alt="Profielfoto"/>
+        <p className="topBarChat__text">{getRoomName(this.props.clickedChatroom, this.props.chatKitUser)}</p>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state =>{
