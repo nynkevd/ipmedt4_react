2deadcc90ddcb1c9491af920ea411b96eb6b37c7
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-06-17 13:51:15 +0200

    geen vrienden/matches/interests paginas

--- src/components/pages/FriendsList.js
+++ src/components/pages/FriendsList.js
@@ -1,6 +1,7 @@
 //React en benodigheden importeren
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import axios from "axios";
+import { Link, Redirect } from 'react-router-dom';
 //Redux importeren
 import { connect } from "react-redux";
 import { changeLoggedIn } from "./../../actions";
@@ -12,13 +13,23 @@ import UserFriendsList from '../friendslist/UserFriendsList';
 //CSS importeren
 import './FriendsList.css';
 
+const api_token = "?api_token=rx7Mi675A1WDEvZPsGnrgvwkCEeOKlrX7rIPoXocluBKnupp9A02OLz7QcSL";
+
 class FriendsList extends React.Component{
   render(){
+    console.log(this.props.allUserFriends);
     return this.props.loggedIn
       ? <div>
         <TopBar />
         <div className="friendsListPageContainer">
+        {this.props.allUserFriends.length > 0 ?
           <UserFriendsList />
+          :
+          <div id="nofriends">
+            <p> Je hebt geen vrienden </p>
+            <Link to="/search"> <img id="sadLogoFriends" src="./img/logoSad.svg" alt="Travel Buddy Sad Logo"/> </Link>
+          </div>
+        }
         </div>
         <BottomNav />
       </div>
@@ -30,6 +41,7 @@ class FriendsList extends React.Component{
 const mapStateToProps = state =>{
   return {
     loggedIn: state.loggedIn,
+    allUserFriends: state.allUserFriends,
   };
 }
 
