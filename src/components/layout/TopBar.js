318db0710a99b4944b2a1f2d1f75807b3700d59a
Lara van Diemen <lara23vd@gmail.com>
2019-06-18 10:36:20 +0200

    Merge was fout gegaan

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -10,8 +10,10 @@ const TopBar = () =>{
       <Link to="/search">
         <img className="topBar__img" src="./img/logoSmall.svg" alt="Travel Buddy Logo"/>
       </Link>
-
       <p className="topBar__text">Travel Buddy</p>
+      <Link to="/" id="link">
+        <img className="topBar__img topBar__img--logout" src="./img/icons/logout.svg" alt="Uitloggen" />
+      </Link>
     </div>
   );
 }
